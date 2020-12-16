45fe5c02c79d6e153537336b34c197388b051d06
AyaaBe95 <75928500+AyaaBe95@users.noreply.github.com>
2020-12-16 11:54:00 +0200

    Create welcome.js

---
+++ welcome.js
@@ -0,0 +1,16 @@
+let  person = prompt("Please enter your name");
+
+if (person != null) {
+  alert("welcome " + person + " in our library ")}
+
+  else {
+      alert (" welcome nobody in our library");
+  }
+
+  
+
+if (confirm("Press a button!")) {
+    txt = "You pressed OK!";
+  } else {
+    txt = "You pressed Cancel!";
+  }
